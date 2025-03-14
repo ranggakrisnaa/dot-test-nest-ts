bf9225a147520fa79e1f444ad685facd56cd3f78
ranggakrisnaa <infinity.duos@gmail.com>
2025-03-15 05:38:21 +0700

    feat: Add health module to check db health and ping

---
+++ src/api/health/health.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  HealthCheck,
+  HealthCheckResult,
+  HealthCheckService,
+  HealthIndicatorFunction,
+  HttpHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+import { AllConfigType } from 'src/config/config.type';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private configService: ConfigService<AllConfigType>,
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  async Health(): Promise<HealthCheckResult> {
+    const list = [() => this.db.pingCheck('database')];
+    return this.health.check(list as HealthIndicatorFunction[]);
+  }
+
+  @Get('ping')
+  PingCheck(): Record<string, any> {
+    return {
+      success: true,
+      statusCode: HttpStatus.OK,
+      message: 'Successfully ping server.',
+    };
+  }
+}
