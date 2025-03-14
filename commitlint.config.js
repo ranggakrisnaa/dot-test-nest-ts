be90249f04fe82717e0eccb925ee58bb56b31aa8
ranggakrisnaa <infinity.duos@gmail.com>
2025-03-14 14:30:20 +0700

    Test Husky dan Lint-Staged

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [2, 'always', [
+        'feat', 
+        'fix', 
+        'docs', 
+        'style', 
+        'refactor', 
+        'test',
+        'chore', 
+        'revert' 
+      ]],
+      'subject-case': [2, 'always', 'sentence-case'], 
+      'header-max-length': [2, 'always', 72], 
+    }
+  };
+  
