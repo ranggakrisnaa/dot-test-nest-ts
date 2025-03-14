734106e7cfaeed239e669ad5f150ecf4ba4e9f42
ranggakrisnaa <infinity.duos@gmail.com>
2025-03-14 15:04:01 +0700

    feat: Add some module 4

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,14 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
