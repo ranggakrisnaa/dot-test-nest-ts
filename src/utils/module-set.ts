913f4f894c966984463d1ed952b5891502a70311
ranggakrisnaa <infinity.duos@gmail.com>
2025-03-15 04:46:30 +0700

    fix: Error path relative with vercel (2)

--- src/utils/module-set.ts
+++ src/utils/module-set.ts
@@ -2,10 +2,10 @@ import { ModuleMetadata } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggerModule } from 'nestjs-pino';
-import { TypeOrmConfigService } from 'src/database/typeorm-config.service';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import appConfig from '../config/app.config';
 import databaseConfig from '../database/config/database.config';
+import { TypeOrmConfigService } from '../database/typeorm-config.service';
 import loggerFactory from './logger-factory';
 
 function generateModulesSet() {
